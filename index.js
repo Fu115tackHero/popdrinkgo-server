4985fa57fa3b9389af8b86de6e1b1477067c2d0f
Cristoval Pratama Siahaan <pratamacristoval@gmail.com>
2025-09-07 22:51:55 +0700

    Update index.js for Vercel

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -9,6 +8,5 @@ app.get('/', (req, res) => {
   res.send('Server PopDrinkGo berjalan!');
 });
 
-app.listen(port, () => {
-  console.log(`Server berjalan di http://localhost:${port}`);
-});
+// Mengekspor aplikasi Express sebagai handler Serverless Function
+module.exports = app;
